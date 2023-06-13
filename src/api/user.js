4821de9edc117ef11aee692331f870e233803d20
lucasBdev <luccacpm@gmail.com>
2023-06-13 18:25:54 -0300

    gettin

---
+++ src/api/user.js
@@ -0,0 +1,15 @@
+export const fetcher = async (url, email, senha) => {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, senha }),
+    });
+
+    if (!response.ok) {
+      throw new Error('Login failed');
+    }
+
+    return response.json();
+  };
